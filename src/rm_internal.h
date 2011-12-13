36c5db50ff77525c2940c2ec0e4f17947179fb33
Mickael <mickaelcamus.mc@gmail.com>
2011-12-13 18:42:52 +0100

    One proposition of headers structure

---
+++ src/rm_internal.h
@@ -0,0 +1,15 @@
+typedef struct rm_fileheader {
+    int recordSize;     // Size of a single record
+    int numberRecords;  // Maximum number of records in a page
+    int nextFreePage;   // Number of the next free page
+    int nextFullPage;   // Number of the next full page
+    int numberPages;    // Number of pages
+} RM_FileHeader;
+
+typedef struct rm_pageheader {
+    int prevPage;       // Number of the previous page
+    int nextPage;       // Number of the next page
+    int numberRecords   // Number of records in the page
+    char* bitmap;       // Pointer to the bitmap
+                        // Size of the bitmap = [RM_FileHeader.numberRecords] bits
+} RM_PageHeader;
