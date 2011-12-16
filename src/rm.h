1127f50c99fd293aadbc3c5c197417cc53a9164f
Mickael <mickaelcamus.mc@gmail.com>
2011-12-16 23:08:36 +0100

    Typo

--- src/rm.h
+++ src/rm.h
@@ -29,7 +29,7 @@ class RM_Record {
     // Both need to be friend so that we can set the private fields
     // directly in these classes
     friend class RM_FileHandle;
-    firend class RM_FileScan;
+    friend class RM_FileScan;
 
 public:
     RM_Record ();
